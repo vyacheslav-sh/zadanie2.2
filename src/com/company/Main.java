0e4210812057c40d89b9865199de54c12b7ed394
vyacheslav-sh <vs8629239@gmail.com>
2021-11-28 15:23:25 +0300

    версия 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int balance = 100;
+    int bonusLimit = 1000;
+    int rublesForBonus = 100;
+    int payment = 1100;
+
+    int bonus = 0;
+    if (payment>bonusLimit)
+        bonus = payment/rublesForBonus;
+
+    int newBalance = balance + payment + bonus;
+    System.out.println("Итого на счёте: " + newBalance);
+    System.out.println("в т.ч. бонусов: " + bonus);
+
+    }
+}
