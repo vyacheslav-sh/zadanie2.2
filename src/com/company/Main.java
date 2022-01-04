d4cf8620918c3f63bab35af3d017119c1728211a
Alexey Boyko <BoykoAA@hq.tatneft.ru>
2022-01-04 12:36:37 +0300

    gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int balance = 100;
-    int bonusLimit = 1000;
-    int rublesForBonus = 100;
-    int payment = 1100;
+        int balance = 100;
+        int bonusLimit = 1000;
+        int rublesForBonus = 100;
+        int payment = 1100;
 
-    int bonus = 0;
-    if (payment>bonusLimit)
-        bonus = payment/rublesForBonus;
+        int bonus = 0;
+        if (payment > bonusLimit) {
+            bonus = payment / rublesForBonus;
+        }
 
-    int newBalance = balance + payment + bonus;
-    System.out.println("Итого на счёте: " + newBalance);
-    System.out.println("в т.ч. бонусов: " + bonus);
+        int newBalance = balance + payment + bonus;
+        System.out.println("Итого на счёте: " + newBalance);
+        System.out.println("в т.ч. бонусов: " + bonus);
 
     }
 }
